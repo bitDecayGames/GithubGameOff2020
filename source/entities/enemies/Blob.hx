ae55c8ff08708aa51b1b8d7f34075fef7dcfc9f0
Logan <monday.hopscotch@gmail.com>
2020-11-21 10:58:26 -0700

    clean up prints in snake, blob

---
+++ source/entities/enemies/Blob.hx
@@ -0,0 +1,102 @@
+package entities.enemies;
+
+import behavior.tree.composite.Parallel;
+import behavior.leaf.util.StartMovementAnimation;
+import flixel.FlxG;
+import behavior.tree.BTContext;
+import behavior.tree.composite.Sequence;
+import behavior.tree.decorator.Repeater;
+import behavior.leaf.PlayerAlive;
+import behavior.leaf.util.Wait;
+import behavior.leaf.util.PickTargetInRange;
+import behavior.leaf.movement.ManhattanPath;
+import behavior.leaf.movement.StraightToTarget;
+import behavior.leaf.TargetPlayer;
+import behavior.tree.BTree;
+import behavior.NavBundle;
+import flixel.util.FlxPath;
+import flixel.FlxObject;
+import flixel.math.FlxPoint;
+import flixel.FlxSprite;
+import states.PlayState;
+
+class Blob extends Enemy {
+    var behavior:BTree;
+
+	public function new(_parentState:PlayState, _player:Player, position:FlxPoint) {
+        super(_parentState, _player, position);
+        path = new FlxPath();
+
+        // frames persecond * move speed
+        baseStats.speed = 15;
+        refresh();
+
+        behavior = new BTree(
+            new Repeater(
+                new Sequence([
+                    new Wait(),
+                    new PickTargetInRange(),
+                    new Parallel([
+                        new StartMovementAnimation(),
+                        new ManhattanPath(),
+                    ]),
+                    new StartMovementAnimation(),
+                ])
+            )
+        );
+        var context = new BTContext();
+        context.set("self", this);
+        context.set("speed", baseStats.speed);
+        context.set("range", 100);
+        context.set("minWait", 0.5);
+        context.set("maxWait", 1);
+        context.set("navBundle", new NavBundle(parentState.currentLevel, player));
+        behavior.init(context);
+
+
+        super.loadGraphic(AssetPaths.blob__png, true, 16, 16);
+
+        var animationSpeed:Int = 15;
+
+        animation.add("walk_up", [1,2,3,4,5], animationSpeed);
+        animation.add("walk_right", [7,8,9,10,11], animationSpeed);
+        animation.add("walk_down", [13,14,15,16,17], animationSpeed);
+        animation.add("walk_left", [7,8,9,10,11], animationSpeed);
+        animation.add("stand_up", [0], animationSpeed);
+        animation.add("stand_right", [0], animationSpeed);
+        animation.add("stand_down", [0], animationSpeed);
+        animation.add("stand_left", [0], animationSpeed);
+
+        animation.play("stand_down");
+
+        setFacingFlip(FlxObject.LEFT, true, false);
+        setFacingFlip(FlxObject.RIGHT, false, false);
+    }
+
+    override public function destroy() {
+        FmodManager.PlaySoundOneShot(FmodSFX.RatDeath);
+        kill();
+    }
+
+	override public function update(delta:Float):Void {
+        super.update(delta);
+
+        if (inKnockback) {
+            path.cancel();
+        }
+
+		if (!inKnockback) {
+            behavior.process(delta);
+
+            if (velocity.x > 0){
+                facing = FlxObject.RIGHT;
+            } else if (velocity.x < 0) {
+                facing = FlxObject.LEFT;
+            } else if (velocity.y < 0) {
+                facing = FlxObject.UP;
+            } else if (velocity.y > 0) {
+                facing = FlxObject.DOWN;
+            }
+        }
+    }
+}

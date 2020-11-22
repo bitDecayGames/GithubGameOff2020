79b0d71e0f752c829461e4c5fcc40ca88bb4f4ff
Tanner <you@example.com>
2020-11-21 22:01:47 -0700

    Shovel at start of game. Can no longer turn while swinging

--- source/interactables/Shovel.hx
+++ source/interactables/Shovel.hx
@@ -3,16 +3,17 @@ package interactables;
 import flixel.math.FlxPoint;
 import entities.Player;
 
-class Axe extends Interactable {
+class Shovel extends Interactable {
     public function new(_position:FlxPoint) {
         super(_position);
-        super.loadGraphic(AssetPaths.axe__png, true, 16, 16);
+        super.loadGraphic(AssetPaths.shovel__png, true, 16, 16);
 
-        name = "Axe";
-        cost = 10;
+        name = "Shovel";
+        cost = 5;
     }
 
     override public function onInteract(_player:Player) {
         super.onInteract(_player);
+        _player.setCanAttack(true);
     }
 }

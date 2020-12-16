88c3319c734da9ff48a721e4d4b0f4a4429ad739
Tanner <Shadoninja@gmail.com>
2020-12-16 16:05:05 -0700

    adding in dialog manager

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 haxelib install haxefmod 
 haxelib git bitlytics https://github.com/bitDecayGames/Bitlytics.git
 haxelib git textpop https://github.com/bitDecayGames/text-pop.git
+haxelib git dialog-manager https://github.com/Tanz0rz/haxe-dialog-manager.git
